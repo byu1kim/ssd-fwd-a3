403828a649cbdb45b690e42de49f8f33fc6aeb8e
Byul Kim <caxbelle@gmail.com>
2023-01-27 10:13:54 -0800

    mobile

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,78 @@
 const body = document.body;
-const menuBtn = document.getElementById("menu-btn");
+const menuBtn = document.getElementsByClassName("menu-btn");
 const nav = document.getElementById("menu");
+const subCha = document.getElementById("sub-channel");
+const subVan = document.getElementById("sub-vancouver");
+
+const btnCha = document.getElementById("channel");
+const btnVan = document.getElementById("vancouver");
+
+const channel = [
+  "DH News",
+  "Dished",
+  "Listed",
+  "Urbanized",
+  "Venture",
+  "Offside",
+  "Curated",
+  "Mapped",
+  "Canada",
+];
+const vancouver = [
+  "Vancouver",
+  "Calgary",
+  "Edmonton",
+  "Montreal",
+  "Toronto",
+  "Seattle",
+  "Portland",
+];
+
+// Add items in divs
+for (let i = 0; i < channel.length; i++) {
+  const li = document.createElement("li");
+  const aa = document.createTextNode(channel[i]);
+  li.appendChild(aa);
+  subCha.appendChild(li);
+}
+
+for (let i = 0; i < vancouver.length; i++) {
+  const li = document.createElement("li");
+  const aa = document.createTextNode(vancouver[i]);
+  li.appendChild(aa);
+  subVan.appendChild(li);
+}
 
 const handleClick = (e) => {
-  body.classList.toggle("show");
+  const show = body.classList.toggle("show");
+  if (!show) {
+    subVan.style.visibility = "inherit";
+  } else {
+  }
 };
 
-menuBtn.addEventListener("click", handleClick);
+for (let i = 0; i < menuBtn.length; i++) {
+  menuBtn[i].addEventListener("click", handleClick);
+}
+
+// Handling menu
+
+btnCha.addEventListener("click", handleChannel);
+btnVan.addEventListener("click", handleVancouver);
+
+function handleChannel() {
+  subCha.style.width = "calc(100vw - 150px)";
+  subVan.style.width = "0";
+  subCha.style.visibility = "visible";
+  subVan.style.visibility = "hidden";
+}
+
+function handleVancouver() {
+  subCha.style.width = "0";
+  subVan.style.width = "calc(100vw - 150px)";
+  subCha.style.visibility = "hidden";
+  subVan.style.visibility = "visible";
+}
 
 // Hidden mobile menu changing window size
 const mql = window.matchMedia("(min-width: 660px)");
